0da47ac7b01343fcb7d5f577a152e2e0200bcaf3
toishin <yobiyoutoishin@gmail.com>
2026-08-14 14:09:46 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!！", intents=intents)
 
 
 def is_admin(ctx: commands.Context) -> bool:
